f967423971014087dc09dde13d01b583ca98ea31
SelimMucharski <mucharski.selim@gmail.com>
2026-03-25 19:27:56 +0100

    Small changes

--- mab_md_tools/pid_tuner/tuner.py
+++ mab_md_tools/pid_tuner/tuner.py
@@ -1,16 +1,22 @@
 import pyCandle
 import time
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
+
+@dataclass
+class PID_params:
+    KP: float
+    KI: float
+    KD: float
+    I_MAX: float
 
 CAN_ID = 316
 TARGET_POSITION = 10.0
-TEST_DURATION = 5.0
+TEST_DURATION = 3.0
 SAMPLING_TIME = 0.01
 
-KP = 10.500000
-KI = 1.5
-KD = 0.00
-I_MAX = 1.0
+positionPID_params = PID_params(100.5, 1.5, 0.0, 1.0)
+velocityPID_params = PID_params(10.100000, 1.000500, 0.0, 0.25)
 
 def run_pid_test():
     candle = pyCandle.attachCandle(pyCandle.CAN_DATARATE_1M, pyCandle.USB)
@@ -22,8 +28,19 @@ def run_pid_test():
 
     print("Inicjalizacja pomyślna. Ustawianie parametrów...")
     
-    md.setMotionMode(pyCandle.POSITION_PID)
-    md.setPositionPIDparam(KP, KI, KD, I_MAX)
+    md.setMotionMode(pyCandle.VELOCITY_PID)
+    
+    md.setPositionPIDparam(positionPID_params.KP,
+                           positionPID_params.KI,
+                           positionPID_params.KD,
+                           positionPID_params.I_MAX
+    )
+    
+    md.setVelocityPIDparam(velocityPID_params.KP,
+                           velocityPID_params.KI,
+                           velocityPID_params.KD,
+                           velocityPID_params.I_MAX
+    )
     
     md.zero()
     time.sleep(0.1)
@@ -41,6 +58,7 @@ def run_pid_test():
             current_elapsed = time.time() - start_time
             
             md.setTargetPosition(TARGET_POSITION)
+            # md.setTargetVelocity(10)
             
             pos, err = md.getPosition()
             # print(err)
@@ -62,7 +80,7 @@ def run_pid_test():
     plt.figure(figsize=(10, 5))
     plt.plot(timestamps, target_positions, 'r--', label='Zadana (Target)')
     plt.plot(timestamps, actual_positions, 'b-', label='Aktualna (Actual)')
-    plt.title(f"Test PID (Kp={KP}, Ki={KI}, Kd={KD})")
+    # plt.title(f"Test PID (Kp={KP}, Ki={KI}, Kd={KD})")
     plt.xlabel("Czas [s]")
     plt.ylabel("Pozycja")
     plt.legend()
